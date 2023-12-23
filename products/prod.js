fdfabe1db5d0e91d72306695e8279d3a4bda3f21
AliElgohary <alielgohary35@gmail.com>
2023-12-24 00:57:38 +0200

    fixing merge problem

--- products/prod.js
+++ products/prod.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let card_btn_user = document.getElementsByClassName("for_user");
 let card_btn_admin = document.getElementsByClassName("for_admin");
 
@@ -35,31 +35,8 @@ if (Object.keys(loggedInUser).length !== 0) {
   loginElement.addEventListener("click", function () {
     window.location.href = "http://127.0.0.1:5500/login/sign-in/index.html";
   });
-=======
-let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser")) || {};
-console.log("Logged-in user:", loggedInUser);
-let card_btn_user=document.getElementsByClassName('for_user');
-let card_btn_admin=document.getElementsByClassName('for_admin');
+}
 
-if (loggedInUser.isAdmin == true) {
-  console.log("user is admin");
-  for(let btn of card_btn_admin){
-    btn.style.display='block';
-   }
-   for(let btn of card_btn_user){
-    btn.style.display='none';
-   }
-}
-if (loggedInUser.isAdmin == false) {
-  console.log("user is user");
-    for(let btn of card_btn_admin){
-      btn.style.display='none';
-    }
-    for(let btn of card_btn_user){
-      btn.style.display='block';
-    }
->>>>>>> refs/remotes/origin/master
-}
 
 // let loginElement = document.getElementById("login");
 
