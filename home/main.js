bd090fb6554e8c62587b8db738e7cac09342a411
AliElgohary <alielgohary35@gmail.com>
2023-12-23 15:40:08 +0200

    Making the login logout button

--- home/main.js
+++ home/main.js
@@ -1,8 +1,23 @@
 let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser")) || {};
 console.log("Logged-in user:", loggedInUser);
-if (loggedInUser.isAdmin == true) {
+
+if (loggedInUser.isAdmin === true) {
   console.log("user is admin");
-}
-if (loggedInUser.isAdmin == false) {
+} else if (loggedInUser.isAdmin === false) {
   console.log("user is not admin");
+}
+
+let loginElement = document.getElementById("login");
+
+if (Object.keys(loggedInUser).length !== 0) {
+  loginElement.innerHTML = `Log out`;
+  loginElement.addEventListener("click", function () {
+    localStorage.removeItem("loggedInUser");
+    location.reload();
+  });
+} else {
+  loginElement.innerHTML = `Log In`;
+  loginElement.addEventListener("click", function () {
+    window.location.href = "http://127.0.0.1:5500/login/sign-in/index.html";
+  });
 }
