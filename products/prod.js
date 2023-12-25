ce8c359852bdfc60e5806bc05686897bbe74ffa6
elgazar <ah152280@gmail.com>
2023-12-26 00:31:48 +0200

    add filteration || search

--- products/prod.js
+++ products/prod.js
@@ -107,7 +107,7 @@ const prodCount=()=>{
 }
 prodCount()
 
-const addProductsToDom = () => {
+const addProductsToDom = (search_case=[]) => {
   products_dom.innerHTML = "";
 
   if (allProd.length > 0) {
@@ -232,4 +232,53 @@ document
   .querySelector(".load_more")
   .addEventListener("click", loadMoreProducts);
 
+// --------------------------------------------------------
+// search
+let search_input=document.getElementById("search_input")
+function search_prod(){
+  search_input.classList.toggle('hide');
+  console.log(allProd)
+}
+search_input.addEventListener('keyup',function(){
+    console.log(this.value)
+    if(search_input.value!==''){
+      let newArray=allProd.filter(e=>e.name.toLowerCase().startsWith(this.value.toLowerCase()));
+      products_dom.innerHTML="";
+      newArray.forEach((prod) => {
+        let new_prod = document.createElement("div");
+        new_prod.classList.add("item");
+        new_prod.innerHTML = `
+                  <img class="item_img" src="${prod.image}" alt="">
+                  <div class="card_text">
+                      <h4>${prod.name}</h4>
+                      <p>${prod.description}</p>
+                      <p>stockQuantity : ${prod.stockQuantity}</>
+                      <div class="rating">
+                      <i class="star fa-solid fa-star star_done"></i>
+                          <i class="star fa-solid fa-star star_done"></i>
+                          <i class="star fa-solid fa-star star_done"></i>
+                          <i class="star fa-regular fa-star"></i>
+                          <i class="star fa-regular fa-star"></i>
+                      </div>
+                      <h3 class="price">${prod.price} $</h3>
+                      ${
+                        loggedInUser.isAdmin
+                          ? `<button class="main_btn card_btn edit-btn">Edit</button>
+                              <button class="main_btn card_btn delete-btn">Delete</button>`
+                          : `<button class="main_btn card_btn add-to-cart-btn">Add to Cart</button>`
+                      }
+                      <span id="prod_code">code: ${prod.id}</span>
+                      <span class="category">category:${prod.category}</span>
+                  </div>
+        `;
+        products_dom.appendChild(new_prod);
+      });
+    }else{
+      addProductsToDom()
+    }
+});
+search_input.addEventListener("blur",function(){
+  this.classList.add("hide");
+  addProductsToDom()
+})
 getData();
